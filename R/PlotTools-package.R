45ecb362ead1ff967c4e72a8b2e02986551c487e
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-05-19 10:12:12 +0100

    Document package

---
+++ R/PlotTools-package.R
@@ -0,0 +1,11 @@
+#' PlotTools
+#'
+#' "PlotTools" is an R package that allows the legends for continuous variables
+#' to be added to plots using the familiar functions of the default 'graphics'
+#' package.  It also includes utility functions to manipulate irregular polygons
+#' and locate their centres.
+#'
+#' Full documentation is available [online](https://ms609.github.io/PlotTools/).
+#'
+#' @keywords internal
+"_PACKAGE"
--- README.md
+++ README.md
@@ -9,7 +9,8 @@
 
 'PlotTools' is an R package that allows the legends for continuous variables
 to be added to plots using the familiar functions of the default 'graphics'
-package.
+package.  It also includes utility functions to manipulate irregular polygons
+and locate their centres.
 
 Install the latest production version from CRAN using
 
